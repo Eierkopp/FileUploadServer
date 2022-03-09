9471aad2e3040799619a1b882082eec9bee74801
Eierkopp <martin@eierkopp.ddnss.org>
2022-03-09 10:46:15 +0100

    HTML fixes

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ dpkg-buildpackage -b --no-sign
 VERSION=$(dpkg-parsechangelog -S version)
 
 dpkg-deb -c ../fus_${VERSION}_all.deb
+
+echo Changelog:
+echo "-- Depperter Depp <depp@eierkopp.ddnss.", $(date -R)
